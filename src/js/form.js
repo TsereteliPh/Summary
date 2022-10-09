1d944421b83282a2b8d32756ecaca4b353983646
TsereteliPh <filippok01001@gmail.com>
2022-10-09 21:11:43 +0300

    Доработан шаблон сообщения

--- src/js/form.js
+++ src/js/form.js
@@ -117,7 +117,12 @@ const URI_API = ` https://api.telegram.org/bot${ token }/sendMessage`;
 form.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
-    let formMessage = `<b>Отклик с сайта!</b>\n <b>Отправитель: </b> ${ this.name.value }\n <b>Компания: </b> ${ this.company_name.value }\n <b>Email: </b> ${ this.email.value }\n <b>Телефон: </b> ${ this.phone.value }\n <b>Дополнительная информация: </b> ${ this.message.value }\n`;
+    let formMessage = `<b>Отклик с сайта!</b>\n`;
+    formMessage += `<b>Отправитель: </b> ${ this.name.value }\n`;
+    formMessage += `<b>Компания: </b> ${ this.company_name.value }\n`;
+    formMessage += `<b>Email: </b> ${ this.email.value }\n`
+    formMessage += `<b>Телефон: </b> ${ this.phone.value }\n`
+    formMessage += `<b>Дополнительная информация: </b> ${ this.message.value }\n`;
     
     if (inputSubmitChecker()) {
         axios.post(URI_API, {
